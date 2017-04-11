d4c4f2a5ba5d912f1bca608f8b89630f4138636d
Peter Stromquist <peter@twistedstream.com>
2017-04-11 00:12:07 -0500

    Demo 4: RS: Configure token validation middleware
    Token must be a symmetrically signed JWT (JWS)

--- demo4_simple-oauth2/SimpleOAuth2.ResourceServer/Startup.cs
+++ demo4_simple-oauth2/SimpleOAuth2.ResourceServer/Startup.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace SimpleOAuth2.ResourceServer
 {
@@ -29,6 +31,16 @@ namespace SimpleOAuth2.ResourceServer
         {
             // Add framework services.
             services.AddMvc();
+
+            // Add authorization policies
+            var issuer = Configuration["TokenValidation:Issuer"];
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("read:timesheets",
+                    policy => policy.Requirements.Add(new HasScopeRequirement("read:timesheets", issuer)));
+                options.AddPolicy("create:timesheets",
+                    policy => policy.Requirements.Add(new HasScopeRequirement("create:timesheets", issuer)));
+            });            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +48,18 @@ namespace SimpleOAuth2.ResourceServer
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            // validate OAuth2 bearer token as a symmetrically-signed JWT
+            var options = new JwtBearerOptions
+            {
+                TokenValidationParameters =
+                {
+                    ValidIssuer = Configuration["TokenValidation:Issuer"],
+                    ValidAudience = Configuration["TokenValidation:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenValidation:SigningKeySecret"]))
+                }
+            };
+            app.UseJwtBearerAuthentication(options);
 
             app.UseMvc();
         }
